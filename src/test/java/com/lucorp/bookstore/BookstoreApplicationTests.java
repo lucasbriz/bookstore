727900a01e824b505c2fbcc24e9fdcb63f698e8f
Lucas Briz <lucasbriz@outlook.com>
2024-11-18 11:59:57 -0300

    created project

---
+++ src/test/java/com/lucorp/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucorp.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
